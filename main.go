16c20ba12037e4013cb488703b9450d29cf49fe4
Vinicius Dias <vi_aleixodias@hotmail.com>
2023-12-26 11:37:40 -0300

    feat: added to  project MVC standart

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	s3Bucket := os.Getenv("TESTE")
+	// secretKey := os.Getenv("SECRET_KEY")
+	fmt.Println(s3Bucket)
+}
