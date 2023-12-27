6b516ce5a313da916b293b4dfab7f59085200a2f
Vinicius Dias <vi_aleixodias@hotmail.com>
2023-12-27 14:09:01 -0300

    feat: added logs

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/vinialeixo/crud-golang/src/configuration/logger"
 	"github.com/vinialeixo/crud-golang/src/controller/routes"
 )
 
 func main() {
+	logger.Info("About to start user aplication")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,16 +1,18 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/vinialeixo/crud-golang/src/configuration/logger"
 	"github.com/vinialeixo/crud-golang/src/controller/dto"
 	"github.com/vinialeixo/crud-golang/src/controller/validation"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
 
+	logger.Info("Init CreateUser controller", zap.String("journey", "createUser"))
 	var userRequest dto.UserRequest
 
 	//&userRequest To be able to modify the original userRequest variable (defined outside the function) with the data from the request,
@@ -18,12 +20,20 @@ func CreateUser(c *gin.Context) {
 	//By using &userRequest, you are passing the memory address of the userRequest variable to the ShouldBind function. This allows ShouldBind to modify the actual variable's content,
 	err := c.ShouldBind(&userRequest)
 	if err != nil {
-		log.Printf("Error trying to marshal object, error=%s\n", err.Error()) //string message
+		logger.Error("Error trying to validate user info ", err, zap.String("journey", "createUser")) //string message
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
 	}
-	fmt.Println(userRequest)
 
+	response := dto.UserResponse{
+		ID:    "teste",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"))
+	c.JSON(http.StatusOK, response)
 }
